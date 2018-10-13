deb09eae50cd87140fe89223de361127afe03a41
cptekam <tekamchandraprakash@gmail.com>
2018-10-13 21:55:28 +0530

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,5 +1,6 @@
 import React , {Component} from 'react';
-
+import Typography from '@material-ui/core/Typography';
+import './Details.css';
 import Header from '../../common/header/Header';
 import moviesData from '../../assets/movieData';
 
@@ -10,7 +11,7 @@ class Details extends Component{
             movie: {}
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
@@ -19,13 +20,23 @@ class Details extends Component{
         console.log(this.state);
     }
     render (){
+        let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
+                        <img src={movie.poster_url} alt={movie.title} />
                     </div>
                     <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title} </Typography>
+                        </div>
+                        <div>
+                            <Typography>
+                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                            </Typography>
+                        </div>
                     </div>
                     <div className="rightDetails">
                     </div>
