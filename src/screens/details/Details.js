284e657b429f5e3174a3b9214f66ca68fa36470a
cptekam <tekamchandraprakash@gmail.com>
2018-10-14 11:20:16 +0530

    Added rating on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -9,11 +9,40 @@ import Home from '../home/Home';
 import YouTube from 'react-youtube';
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
 class Details extends Component{
     constructor() {
         super();
         this.state = {
-            movie: {}
+            movie: {},
+            starIcons: [
+                {
+                    id: 1,
+                    stateId: "star1",
+                    color: "black"
+                },
+                {
+                    id: 2,
+                    stateId: "star2",
+                    color: "black"
+                },
+                {
+                    id: 3,
+                    stateId: "star3",
+                    color: "black"
+                },
+                {
+                    id: 4,
+                    stateId: "star4",
+                    color: "black"
+                },
+                {
+                    id: 5,
+                    stateId: "star5",
+                    color: "black"
+                }
+            ]
         }
     }
     backToHomeHandler = () => {
@@ -31,6 +60,21 @@ class Details extends Component{
     artistClickHandler = (wikiUrl) => {
         window.location = wikiUrl;
 }
+    starClickHandler = (id) => {
+        let starIconList = [];
+        for (let star of this.state.starIcons) {
+            let starNode = star;
+            if (star.id <= id) {
+                starNode.color = "yellow"
+            }
+            else {
+                starNode.color = "black";
+            }
+            starIconList.push(starNode);
+        }
+        this.setState({ starIcons: starIconList });
+    }
+
     render (){
         let movie = this.state.movie;
         const opts = {
@@ -85,6 +129,16 @@ class Details extends Component{
                         </div>
                     </div>
                     <div className="rightDetails">
+                        <Typography>
+                            <span className="bold">Rate this movie: </span>
+                        </Typography>
+                        {this.state.starIcons.map(star => (
+                            <StarBorderIcon
+                                className={star.color}
+                                key={"star" + star.id}
+                                onClick={() => this.starClickHandler(star.id)}
+                            />
+                        ))}
                         <div  className="bold marginBottom16 marginTop16">
                             <Typography><span className="bold"> Artists:</span></Typography>
                         </div>
